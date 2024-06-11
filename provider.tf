3e06e4ff61e00b7a49585d340f60f217575d935e
ymkgithub <147030007+ymkgithub@users.noreply.github.com>
2024-06-11 16:32:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,15 @@
 terraform {
   required_version = ">= 1.7"
 
-required_providers {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0"
     }
   }
-  
-# Adding Backend as S3 for Remote State Storage
-backend "s3" {
+
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
     bucket = "lambda-tf-bucket-github-runner"
     key    = "Lambda_TF_State/terraform.tfstate"
     region = "us-east-1"
@@ -17,5 +17,5 @@ backend "s3" {
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
