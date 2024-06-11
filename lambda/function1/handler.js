60be68398059049c6f7959c1941c540af442f033
ymkgithub <147030007+ymkgithub@users.noreply.github.com>
2024-06-11 16:44:07 +0530

    Update handler.js

--- lambda/function1/handler.js
+++ lambda/function1/handler.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify('Hello from mahesh kumar Function 1!'),
+        body: JSON.stringify('Hello from mahesh kumar to Function 1!'),
     };
 };
