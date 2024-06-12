93856304e8891b2a3cdddfbdbd7f3a2e463626a7
saravanabics <saravanakumar@bicsglobal.com>
2024-06-12 18:36:40 +0530

    added all files

--- provider.tf
+++ provider.tf
@@ -1,13 +1,17 @@
+# Terraform Block
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.7" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0"
+      version = ">= 5.0"
     }
   }
-}
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "cy-rl-seam-tf-bucket"
+    key    = "cy-rl-seam/terraform.tfstate"
+    region = "us-east-1"
 
-provider "aws" {
-  region = "us-east-1"
+  }
 }
