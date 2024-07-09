13ad03d2ec136ba1d29918c84f98fde1449e30ea
saravanabics <saravanakumar@bicsglobal.com>
2024-07-09 16:09:24 +0530

    modified function 1 file

--- lambda/function1/handler.js
+++ lambda/function1/handler.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify('Say Hi 123456789 and Hello from mahesh kumar to Function 1!'),
+        body: JSON.stringify('Testing the provisioned concurrency for Function 1!'),
     };
 };
