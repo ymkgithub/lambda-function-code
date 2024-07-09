564aba174fe62ca976e36f912841aaa78aae9c63
saravanabics <saravanakumar@bicsglobal.com>
2024-07-09 16:22:37 +0530

    function 1changes

--- lambda/function1/handler.js
+++ lambda/function1/handler.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify('Testing the provisioned concurrency for Function 1!'),
+        body: JSON.stringify('Testing the provisioned concurrency 2nd time for Function 1!'),
     };
 };
