db5d312bf36bd889f1f18d4254dc820208cb3d60
saravanabics <saravanakumar@bicsglobal.com>
2024-07-09 17:59:07 +0530

    adf

--- lambda/function1/handler.js
+++ lambda/function1/handler.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify('Testing the provisioned concurrency time for Function 1!.  testing Added resolution'),
+        body: JSON.stringify('Testing the provisioned concurrency time for Function 1!. testing Added resolution'),
     };
 };
