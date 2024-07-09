aed8a8dee492921def952a91f168290dafc298c5
saravanabics <saravanakumar@bicsglobal.com>
2024-07-09 17:52:07 +0530

    running immediate build

--- lambda/function2/handler.js
+++ lambda/function2/handler.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify('Testing the provisioned concurrency for Function 2! Added resolution'),
+        body: JSON.stringify('Testing the provisioned concurrency for Function 2! testing the Added resolution'),
     };
 };
